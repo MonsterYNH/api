44980201803694d047342e9eac1033e8590d69c2
yangniuhong <yangniuhong@monster.com>
2021-06-27 09:39:20 +0800

    change dir

--- gencode.sh
+++ gencode.sh
@@ -1,2 +1,2 @@
-protoc -I . --go_out=plugins=grpc:. --grpc-gateway_out=. v1/health/*.proto
-protoc -I . --go_out=plugins=grpc:. --grpc-gateway_out=. v1/oauth2/*.proto
+protoc -I . --go_out=plugins=grpc:. --grpc-gateway_out=. protos/v1/health/*.proto
+protoc -I . --go_out=plugins=grpc:. --grpc-gateway_out=. protos/v1/oauth2/*.proto
